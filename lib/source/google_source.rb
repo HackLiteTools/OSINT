a543ff5ad4593e243cd9915f2bab925fa5bc4752
Daniel Krňák <daniel.krnak@spaceflow.io>
2022-01-17 10:10:18 +0100

    Email-3: Add language support into Google source

--- lib/source/google_source.rb
+++ lib/source/google_source.rb
@@ -5,18 +5,20 @@ require "open-uri"
 module Source
   class GoogleSource
     TOP_LEVEL_DOMAIN = "com"
+    LANGUAGE = "en"
     ENCODING = "UTF-8"
     PAGINATION = (10..50).step(10)
 
-    def initialize(top_level_domain = TOP_LEVEL_DOMAIN)
+    def initialize(top_level_domain = TOP_LEVEL_DOMAIN, language = LANGUAGE)
       @top_level_domain = top_level_domain
+      @language = language
       @base_url = "https://www.google.#{top_level_domain}/search"
       @results = []
     end
 
     def results(phrase, regex, encoding = ENCODING)
       PAGINATION.each do |page|
-        URI.parse(@base_url + "?q=#{phrase}&hl=en&lr=&ie=#{encoding}&start=#{page}&sa=N").open do |file|
+        URI.parse(@base_url + "?q=#{phrase}&lr=lang_#{@language}&ie=#{encoding}&start=#{page}").open do |file|
           file.each_line do |line|
             result = line.match(regex)
             @results << result.to_s if result
