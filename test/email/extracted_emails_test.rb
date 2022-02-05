1bd1fc0820338c893b3f1fcab1d3bdffda11cadd
Daniel Krňák <daniel.krnak@spaceflow.io>
2022-02-06 00:54:02 +0100

    Change assertion and format in test

--- test/email/extracted_emails_test.rb
+++ test/email/extracted_emails_test.rb
@@ -7,16 +7,21 @@ require "./lib/email/extracted_emails"
 
 class ExtractedEmailsTest < Minitest::Test
   def test_finding_emails_in_source
-    assert_match(
-      /@protonmail.com/,
-      Email::ExtractedEmails.new("protonmail.com", Source::GoogleSource.new).list.to_s
-    )
+    Email::ExtractedEmails.new("protonmail.com", Source::GoogleSource.new).list.each do |email|
+      assert_match(/@protonmail.com/, email)
+    end
   end
 
   def test_finding_in_results
     assert_equal(
-      %w[Aztecdecrypt@protonmail.com mmingard@protonmail.com Corpseworm@protonmail.com nrn_1@protonmail.com illinoispatriot@protonmail.com Decryptdocs@protonmail.com nicecupojoe@protonmail.com],
-      Email::ExtractedEmails.new("protonmail.com", Source::LocalSource.new(format("%s//fixtures/1-protonmail.html", File.dirname(__FILE__)))).list
+      %w[
+        Aztecdecrypt@protonmail.com mmingard@protonmail.com Corpseworm@protonmail.com nrn_1@protonmail.com
+        illinoispatriot@protonmail.com Decryptdocs@protonmail.com nicecupojoe@protonmail.com
+      ],
+      Email::ExtractedEmails.new(
+        "protonmail.com",
+        Source::LocalSource.new(format("%s//fixtures/1-protonmail.html", File.dirname(__FILE__)))
+      ).list
     )
   end
 end
